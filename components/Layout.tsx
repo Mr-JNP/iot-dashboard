227d8245f0a529ea85190a01f6758890ecb9f2b2
Nontapat Pintira <nontapat.pin@gmail.com>
2020-12-24 21:35:20 +0700

    Init Project

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,41 +1,38 @@
-import React, { ReactNode } from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import React, { ReactNode } from "react";
+import Head from "next/head";
+import { Layout, Typography } from "antd";
+
+const { Header, Content, Footer } = Layout;
+const { Title } = Typography;
 
 type Props = {
-  children?: ReactNode
-  title?: string
-}
+  children?: ReactNode;
+  title?: string;
+};
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
+const Index = ({ children, title = "This is the default title" }: Props) => (
   <div>
     <Head>
       <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta charSet='utf-8' />
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
     </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
+    <Layout className='layout'>
+      <Header>
+        <div className='logo' />
+        <Title style={{ color: "#eff2f5", margin: "8px" }}>
+          Mar Mar Mink | Dashboard
+        </Title>
+      </Header>
+      <Content style={{ padding: "0 50px" }}>
+        <div className='site-layout-content'>{children}</div>
+      </Content>
+      <Footer style={{ textAlign: "center" }}>
+        Ant Design ©2018 Created by Ant UED
+      </Footer>
+    </Layout>
+    ,
   </div>
-)
+);
 
-export default Layout
+export default Index;
